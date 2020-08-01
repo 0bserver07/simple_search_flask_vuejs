a82d28cce960b4670e54de365c436a2b78577949
Yad Konrad <yad.konrad@gmail.com>
2020-07-31 20:06:17 -0700

    slowly adding data parsers

---
+++ db/data_generator.py
@@ -0,0 +1,11 @@
+
+
+def bigQuery_loader(file_path):
+    """[summary]
+        loads a json file with bigQ data extracted via file_path
+    Args:
+        file_path ([str]): json file's file_path
+    """
+
+
+    return data
